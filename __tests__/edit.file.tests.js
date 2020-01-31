84f46b38ded8f83d53c97c36be44d257605c4129
Ayman <aymankhawaldeh8@gmail.com>
2020-01-31 20:01:44 +0200

    fixed every thing

--- __tests__/edit.file.tests.js
+++ __tests__/edit.file.tests.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 'use strict';
 
 jest.mock('fs');
@@ -5,15 +6,15 @@ jest.mock('fs');
 const reader = require('../lib/edit-file');
 
 describe('file reader module as callback',()=>{
-    let file =`${__dirname}/data/person.json`;
-    it('when given a bad file, return an error',()=>{
-        reader.readerFile(file,(err,data)=>{
-            expect(err).toBe(undefined);
-        })
-    })
-    it('when given the same file, return content',()=>{
-        reader.readerFile(file,(err,data)=>{
-            expect(typeof data).toBe('string');
-        })
-    })
-})
+  let file =`${__dirname}/data/person.json`;
+  it('when given a bad file, return an error',()=>{
+    reader.readerFile(file,(err,data)=>{
+      expect(err).toBe(undefined);
+    });
+  });
+  it('when given the same file, return content',()=>{
+    reader.readerFile(file,(err,data)=>{
+      expect(typeof data).toBe('string');
+    });
+  });
+});
--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+/* eslint-disable no-unused-vars */
 'use strict';
 
 const fs = require('fs');
 const util = require('util');
 
-const reader = require('./lib/edit-file')
+const reader = require('./lib/edit-file');
 
 let file =`${__dirname}/data/person.json`;
 
 reader.readerFile(file, (err, data) => {
-    if (err) { throw err; }
-    console.log('Callback from module', data);
-  });
+  if (err) { throw err; }
+  console.log('Callback from module', data);
+});
